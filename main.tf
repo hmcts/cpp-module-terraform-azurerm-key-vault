d99ae1ae754e02a989d5c5b98621e36252f08f2e
Dawid Strozak <dawid.strozak@hmcts.net>
2025-08-14 15:31:01 +0100

    Add conditional to private dns zone group so it is not created when the private dns zone doesnt exist

--- main.tf
+++ main.tf
@@ -86,11 +86,15 @@ resource "azurerm_private_endpoint" "endpoint-vault" {
     subresource_names              = ["vault"]
     is_manual_connection           = false
   }
-  private_dns_zone_group {
-    count                = var.enable_data_lookup ? 1 : 0
-    name                 = "dns-zone-group-kv"
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_kv[0].id]
+
+  dynamic "private_dns_zone_group" {
+    for_each = var.enable_data_lookup ? [true] : []
+    content {
+      name                 = "dns-zone-group-kv"
+      private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_kv[0].id]
+    }
   }
+
   tags = var.tags
 }
 
@@ -109,7 +113,6 @@ resource "azurerm_private_endpoint" "external_endpoint_vault" {
   }
 
   private_dns_zone_group {
-    count                = var.enable_data_lookup ? 1 : 0
     name                 = "${var.name}-${each.value.private_dns_resource_group_name}-dns-zone-group"
     private_dns_zone_ids = [data.azurerm_private_dns_zone.dns_external[each.key].id]
   }
